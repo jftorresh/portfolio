437027a72fd1156fb59401a81e02dbc1fa83c242
David Torres <dftp93@gmail.com>
2020-05-08 20:28:04 -0500

    Added layer of main menu

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -1,1 +1,11 @@
-console.log("Estable");
+var openMenu = document.querySelector("#open-menu").addEventListener("click", function (e) {
+  e.preventDefault();
+  var menu = document.getElementById("menu");
+  menu.classList.add("active");
+});
+
+var openMenu = document.querySelector("#close-menu").addEventListener("click", function (e) {
+  e.preventDefault();
+  var menu = document.getElementById("menu");
+  menu.classList.remove("active");
+});
